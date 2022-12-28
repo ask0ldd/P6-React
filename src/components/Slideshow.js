b41badb755ee162acec76e90e6da167ab5ce7a97
Cedric B-Eff <cedrikopenclass@gmx.com>
2022-12-29 00:09:07 +0100

    before installing proptypes

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,12 +1,27 @@
 import '../styles/Slideshow.css'
+import { useState } from 'react'
 
 const Slideshow = (props) => {
 
     const imageUrl = ["https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg","https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-2.jpg","https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-3.jpg"]
 
+    const [indexImg, setIndexImg] = useState(0);
+
+    function nextImg() {
+        indexImg < imageUrl.length-1 ? setIndexImg(indexImg + 1) : setIndexImg(0)
+    }
+
+    function prevImg() {
+        indexImg !== 0 ? setIndexImg(indexImg - 1) : setIndexImg(imageUrl.length-1)
+    }
+
     return (
-    <section>
-        <img src={imageUrl[0]} alt="temp"/>
+    <section className='slideshow'>
+        <img src={imageUrl[indexImg]} alt="temp"/>
+        <div className="buttons-container">
+            <div onClick={nextImg}></div>
+            <div onClick={prevImg}></div>
+        </div>
     </section>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ function App() {
         <Gallery/>
       </main>
       <Footer/>
-    </div>
+      </div>
   );
 }
 
