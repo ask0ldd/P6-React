a429cbabc09f729756a89e55d1c8765e8b03a026
Cedric B-Eff <cedrikopenclass@gmx.com>
2022-12-30 01:35:44 +0100

    responsive collapses

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -2,19 +2,24 @@ import '../styles/Collapse.css'
 import { useState } from 'react'
 import arrowUp from '../assets/collapse-up.svg'
 import arrowDown from '../assets/collapse-down.svg'
+import { useLocation } from 'react-router-dom'
+
 
 const Collapse = (props) => {
 
     const [bodyDisplay, updateBodyDisplay] = useState(false)
 
-    //console.log(typeof(props.body))
+    //check if uselocation contains hebergement or apropos and assign expected fontsize
+    console.log(useLocation())
+    const fontSize = useLocation().pathname.includes("hebergement") ? ['rental-heading', 'rental-body'] : ['apropos-heading', 'apropos-body']
+
     return (
     <article className="collapse">
-        <div onClick={()=>updateBodyDisplay(!bodyDisplay)} className="collapse__heading" style={{fontSize:props.fontsize}}>
+        <div onClick={()=>updateBodyDisplay(!bodyDisplay)} className={'collapse__heading '+fontSize[0]}>
             <div>{props.heading}</div><img src={bodyDisplay === true ? arrowUp : arrowDown} alt="arrow" />
         </div>
         {bodyDisplay === true &&
-        <div className="collapse__body" style={{fontSize:props.fontsize}} dangerouslySetInnerHTML={{__html: props.body}} />}
+        <div className={'collapse__body '+fontSize[1]} dangerouslySetInnerHTML={{__html: props.body}} />}
     </article>
     )
 }
