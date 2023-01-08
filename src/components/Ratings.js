fea6dffdb4e83c85db227286a55ab9c6a6560be4
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-01-08 09:35:16 +0100

    cleaning comments

--- src/components/Ratings.js
+++ src/components/Ratings.js
@@ -28,24 +28,3 @@ const Ratings = (props) => {
 }
  
 export default Ratings
-
-/*
-props 
-state
-carousel
-collapse
-react navigation
-create react app
-
-Déroulement soutenance P6 :
-4:07
-Durant la présentation orale, l’évaluateur interprétera le rôle du CTO. La soutenance est structurée de la manière suivante :
-Présentation des livrables (15 minutes)
-Présentation des différentes pages, avec la navigation entre elles, et les interactions avec tous les éléments interactifs.
-Parcours des fichiers de code avec le mentor. Mise en avant des différents composants React.
-Discussion (10 minutes)
-Le mentor, qui joue le rôle du CTO, posera des questions sur la méthodologie adoptée et sur les livrables.
-Le mentor challengera au maximum les décisions techniques que vous avez prises ; il faudra donc que vous soyez capable de les justifier, et de défendre votre travail.
-Débriefing (5 minutes)
-À la fin de la soutenance, l'évaluateur arrêtera de jouer le rôle du CTO pour vous permettre de débriefer ensemble.
-*/
