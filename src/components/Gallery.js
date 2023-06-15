d294547470ca706219d4ffd2cc9933d3446860a6
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-06-15 04:34:41 +0200

    props updates

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,15 +3,15 @@ import ImmoCard from './ImmoCard'
 import { Link } from 'react-router-dom'
 import { ErrorBox } from './ErrorBox'
 
-const Gallery = (props) => {
+const Gallery = ({dataset, error, loadingState}) => {
 
     return (
     <section id="gallery">
         {
-            (props.dataset && /* if datas got fetched */
-                props.dataset.map((logement) => (<Link className="anchor" to={"hebergement/"+logement.id} key={logement.id}>
+            (dataset && /* if datas got fetched */
+                dataset.map((logement) => (<Link className="anchor" to={"hebergement/"+logement.id} key={logement.id}>
                     <ImmoCard title={logement.title} cover={logement.cover}/></Link>)))
-            || ((props.error && !props.loadingState) && /* !props.loadingState > don't show the error div when no real error, ie : it's only loading */
+            || ((error && !loadingState) && /* !props.loadingState > don't show the error div when no real error, ie : it's only loading */
                 <ErrorBox />)}
     </section>
     )
